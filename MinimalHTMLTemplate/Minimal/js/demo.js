8d1606389e04dd0bd5b6c9114fcc29d6eab59077
jthoms19 <johan.thomsen@hotmail.com>
2020-02-17 22:19:56 +0100

    xd

--- MinimalHTMLTemplate/Minimal/js/demo.js
+++ MinimalHTMLTemplate/Minimal/js/demo.js
@@ -1,11 +1,21 @@
 'use strict'
 
-async function getSumonnerInfo(SummonerName){
-    const result = await fetch(`https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/FroggenCosplayer?api_key=RGAPI-e46ad9d5-d060-4960-a9f2-8ede969513d9`);
+/*async function getSumonnerInfo(SummonerName){
+    const result = await fetch(`https://jsonplaceholder.typicode.com/users`);
     const data   = await result.json();
     console.log(data);
 }
 
 getSumonnerInfo('FroggenCosplayer');
-console.log("JS er klar!");
+console.log("JS er klar!");*/
 
+
+
+
+async function getSumonnerInfo(SummonerName){
+  const result = await fetch(`https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${SummonerName}?api_key=RGAPI-e46ad9d5-d060-4960-a9f2-8ede969513d9`);
+  const data   = await result.json();
+  console.log(data);
+}
+
+getSumonnerInfo('FroggenCosplayer');
