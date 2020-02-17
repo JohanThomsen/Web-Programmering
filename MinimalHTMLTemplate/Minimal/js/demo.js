ed3819584c900398e68ed804e01a421341cdda61
jthoms19 <johan.thomsen@hotmail.com>
2020-02-17 23:22:42 +0100

    We hype

--- MinimalHTMLTemplate/Minimal/js/demo.js
+++ MinimalHTMLTemplate/Minimal/js/demo.js
@@ -13,14 +13,22 @@ console.log("JS er klar!");*/
 
 
 async function getSummonerInfo(SummonerName){
-  const SummmonerResult = await fetch(`https://cors-anywhere.herokuapp.com/https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${SummonerName}?api_key=RGAPI-e46ad9d5-d060-4960-a9f2-8ede969513d9`);
-  const SummonerData    = await SummmonerResult.json();
-  const rankedResult    = await fetch(`https://cors-anywhere.herokuapp.com/https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/8gYVa5UY42YCmqjIfl8bV_WcUSkwug_oLEu4m9faBFQ-DGE?api_key=RGAPI-e46ad9d5-d060-4960-a9f2-8ede969513d9`);
-  const rankedData      = await rankedResult.json();
+  let index = -1;
+  let SummmonerResult = await fetch(`https://cors-anywhere.herokuapp.com/https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${SummonerName}?api_key=RGAPI-e46ad9d5-d060-4960-a9f2-8ede969513d9`);
+  let SummonerData    = await SummmonerResult.json();
+  let rankedResult    = await fetch(`https://cors-anywhere.herokuapp.com/https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/${SummonerData.id}?api_key=RGAPI-e46ad9d5-d060-4960-a9f2-8ede969513d9`);
+  let rankedData      = await rankedResult.json();
   console.log(rankedData);
   console.log(SummonerData);
-  console.log(`Summoner ${SummonerData.name} is level ${SummonerData.summonerLevel}. This account is also ${rankedData[1].tier} ${rankedData[1].rank}.
-  This account has ${rankedData[1].leaguePoints} LP, with a winrate of ${(rankedData[1].losses / rankedData[1].wins) * 100}%.`);
+
+  if(rankedData[0].queueType === "RANKED_SOLO_5x5"){
+    index = 0;
+  } else if (rankedData[1].queueType === "RANKED_SOLO_5x5"){
+    index = 1;
+  }
+
+  console.log(`Summoner ${SummonerData.name} is level ${SummonerData.summonerLevel}. This account is also ${rankedData[index].tier} ${rankedData[index].rank}, in Solo q.
+  This account has ${rankedData[index].leaguePoints} LP, with a winrate of ${(rankedData[index].losses / rankedData[index].wins) * 100}%.`);
   return SummonerData;
 }
 
