88680c1dc2b79adb4cc51788545a6fec50107523
jthoms19 <johan.thomsen@hotmail.com>
2020-02-17 19:01:37 +0100

    Reeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee RIot API

--- MinimalHTMLTemplate/Minimal/js/demo.js
+++ MinimalHTMLTemplate/Minimal/js/demo.js
@@ -1,19 +1,11 @@
 'use strict'
-//SEE: https://javascript.info/strict-mode
 
+async function getSumonnerInfo(SummonerName){
+    const result = await fetch(`https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/FroggenCosplayer?api_key=RGAPI-e46ad9d5-d060-4960-a9f2-8ede969513d9`);
+    const data   = await result.json();
+    console.log(data);
+}
+
+getSumonnerInfo('FroggenCosplayer');
 console.log("JS er klar!");
 
-let size = 8;
-let chessArray = [];
-
-  for(i = 0; i <= size * size; i++)
-  {
-    if(i % 2)
-    {
-      chessArray.push("");
-    } else {
-      chessArray.push("#");
-    }
-  }
-
-console.log(chessArray);
