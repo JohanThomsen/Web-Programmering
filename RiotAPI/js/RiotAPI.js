66e58241b08b5678df42a1b645e24b3b47bbf6ed
jthoms19 <johan.thomsen@hotmail.com>
2020-02-26 00:07:45 +0100

    Wack

--- RiotAPI/js/RiotAPI.js
+++ RiotAPI/js/RiotAPI.js
@@ -1,17 +1,17 @@
-'use strict'
+//'use strict'
 
 const TIMEINWEEK = 604000000;
-let APIKey = 'RGAPI-cadd4e9b-c828-40b5-b045-dc5ed3d37b05';
+let APIKey = 'RGAPI-ba7dfaff-56b6-4ef1-80e5-c6a655e09984';
 let summonerData, rankedData, matchHistoryData, championsData, GchampName;
-let matchDataArr = [];
+//let matchDataArr = [];
 let tempDate = new Date();
 let weekAgo = tempDate.getTime() - TIMEINWEEK;
 let numberOfGames = -1;
+console.log(weekAgo);
 
 
 
-
-async function getSummonerInfo(SummonerName, champName){
+async function getSummonerInfo(SummonerName, queueKey, champName){
   GchampName = champName;
 
   let championsResult    = await fetch(`../Champions.json`);
@@ -24,11 +24,17 @@ async function getSummonerInfo(SummonerName, champName){
       rankedData         = await rankedResult.json();
   //  console.log(rankedData);
 
-  if (champName) {
-    let matchHistoryResult = await fetch(`https://cors-anywhere.herokuapp.com/https://euw1.api.riotgames.com/lol/match/v4/matchlists/by-account/${summonerData.accountId}?champion=${championsData.data[champName].key}&queue=420&beginTime=${weekAgo}&api_key=${APIKey}`);
+  if (champName && queueKey) {
+    let matchHistoryResult = await fetch(`https://cors-anywhere.herokuapp.com/https://euw1.api.riotgames.com/lol/match/v4/matchlists/by-account/${summonerData.accountId}?champion=${championsData.data[champName].key}&queue=${queueKey}&beginTime=${weekAgo}&api_key=${APIKey}`);
+    matchHistoryData       = await matchHistoryResult.json();
+  } else if (!champName && queueKey){
+    let matchHistoryResult = await fetch(`https://cors-anywhere.herokuapp.com/https://euw1.api.riotgames.com/lol/match/v4/matchlists/by-account/${summonerData.accountId}?queue=${queueKey}&beginTime=${weekAgo}&api_key=${APIKey}`);
+    matchHistoryData       = await matchHistoryResult.json();
+  } else if (champName && !queueKey){
+    let matchHistoryResult = await fetch(`https://cors-anywhere.herokuapp.com/https://euw1.api.riotgames.com/lol/match/v4/matchlists/by-account/${summonerData.accountId}?champion=${championsData.data[champName].key}&beginTime=${weekAgo}&api_key=${APIKey}`);
     matchHistoryData       = await matchHistoryResult.json();
   } else {
-    let matchHistoryResult = await fetch(`https://cors-anywhere.herokuapp.com/https://euw1.api.riotgames.com/lol/match/v4/matchlists/by-account/${summonerData.accountId}?queue=420&beginTime=${weekAgo}&api_key=${APIKey}`);
+    let matchHistoryResult = await fetch(`https://cors-anywhere.herokuapp.com/https://euw1.api.riotgames.com/lol/match/v4/matchlists/by-account/${summonerData.accountId}?beginTime=${weekAgo}&api_key=${APIKey}`);
     matchHistoryData       = await matchHistoryResult.json();
   }
   
@@ -38,7 +44,7 @@ async function getSummonerInfo(SummonerName, champName){
   } else {
     numberOfGames = matchHistoryData.totalGames;
   }
-
+  let matchDataArr = [];
   for (let i = 1; i <= numberOfGames; i++) {
     let matchIndex    = matchHistoryData.totalGames - i;
     console.log(matchIndex);
@@ -59,10 +65,6 @@ getSummonerInfo('FroggenCosplayer')
     rankedIndex = 1;
   }
 
-  console.log(`Summoner ${summonerData.name} is level ${summonerData.summonerLevel}. This account is also ${rankedData[rankedIndex].tier} ${rankedData[rankedIndex].rank}, in Solo q.
-  This account has ${rankedData[rankedIndex].leaguePoints} LP, with a winrate of ${(rankedData[rankedIndex].wins / (rankedData[rankedIndex].losses + rankedData[rankedIndex].wins)) * 100}%.
-  ${summonerData.name} Has played ${matchHistoryData.totalGames} ranked solo q games in the last 7 days`);  //${GchampName}
-
   document.getElementById("summonerName").innerHTML = `${summonerData.name}`;
 
   document.getElementById("rankBlock").innerHTML = ` <p>Rank: ${rankedData[rankedIndex].tier} ${rankedData[rankedIndex].rank} ${rankedData[rankedIndex].leaguePoints} LP</p>`
@@ -70,10 +72,42 @@ getSummonerInfo('FroggenCosplayer')
   document.getElementById("winrateBlock").innerHTML = ` <p>Winrate: ${(rankedData[rankedIndex].wins / (rankedData[rankedIndex].losses + rankedData[rankedIndex].wins)) * 100}%</p>`
 })
 .catch(ErrorCode => {
-  console.log(`Øv fejlkode (${ErrorCode})`);
-  document.getElementById("summonerName").innerHTML = `Loading Failed. Error : (${ErrorCode})`;
+console.log(`Øv fejlkode (${ErrorCode})`);
+document.getElementById("summonerName").innerHTML = `Error : (${ErrorCode})`;
 });
 
 
-//      <label for="champName">Specific Champion</label><br>
-//<input type="text" id="champName" name="champName"><br>
+function inputFilterData(){
+  let searchFilter = {
+    queueSearch: document.querySelector('input[name="queue"]:checked').id,
+    champSearch: document.getElementById('champName').value,
+    summonerNameSearch: document.getElementById('summonerNameInput').value,
+  }
+
+  let queueKey = queueSearch(searchFilter.queueSearch);
+  console.log(queueKey);
+
+  getSummonerInfo(`${searchFilter.champSearch}`, queueKey, `${searchFilter.champSearch}`)
+  console.log(searchFilter);
+}
+
+function queueSearch(textQueue){
+  switch (textQueue) {
+    case "soloQ":
+      return 420;
+      break;
+
+    case "flexQ":
+      return 440;
+      break;
+
+    case all:
+      return "";
+      break;
+
+    default:
+      return "";
+      break;
+  }
+}
+
